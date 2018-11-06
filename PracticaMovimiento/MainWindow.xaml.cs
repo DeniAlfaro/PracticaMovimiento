cdc9fd772bdefb55fc82b96667edc8d587ee5708
Deni Alfaro <denilorenaalfaro@hotmail.com>
2018-11-06 08:37:55 -0700

    threading

--- PracticaMovimiento/MainWindow.xaml.cs
+++ PracticaMovimiento/MainWindow.xaml.cs
@@ -13,6 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// librerias para multiprocesamiento
+using System.Threading;
+using System.Diagnostics;
+
 namespace PracticaMovimiento
 {
     /// <summary>
@@ -20,18 +24,70 @@ namespace PracticaMovimiento
     /// </summary>
     public partial class MainWindow : Window
     {
+        Stopwatch stopwatch;
+        TimeSpan tiempoAnterior;
+
         public MainWindow()
         {
             InitializeComponent();
             miCanvas.Focus();
+
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            tiempoAnterior = stopwatch.Elapsed;
+            
+            // 1. establecer instrucciones
+            ThreadStart threadStart = new ThreadStart(moverEnemigos);
+            // 2. inicializar el Thread
+            Thread threadMoverEnemigos = new Thread(threadStart);
+            // 3. ejecutar el Thread
+            threadMoverEnemigos.Start();
+
+        }
+
+        void moverEnemigos()
+        {
+            while (true)
+            {
+                Dispatcher.Invoke(
+                () =>
+                {
+                    var tiempoActual = stopwatch.Elapsed;
+                    var deltaTime = tiempoActual - tiempoAnterior;
+
+                    double leftTumblrActual = Canvas.GetLeft(imgTumblr);
+                    Canvas.SetLeft(imgTumblr, leftTumblrActual - (90 * deltaTime.TotalSeconds));
+                    if(Canvas.GetLeft(imgTumblr) <= -100)
+                    {
+                        Canvas.SetLeft(imgTumblr, 800);
+                    }
+                    tiempoAnterior = tiempoActual;
+                }
+                );
+            }
         }
 
         private void miCanvas_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Up)
             {
-                double topTumblrActual = Canvas.GetTop(imgTumblr);
-                Canvas.SetTop(imgTumblr, topTumblrActual - 15);
+                double topCelularActual = Canvas.GetTop(imgCelular);
+                Canvas.SetTop(imgCelular, topCelularActual - 15);
+            }
+            if (e.Key == Key.Down)
+            {
+                double bottomCelularActual = Canvas.GetTop(imgCelular);
+                Canvas.SetTop(imgCelular, bottomCelularActual + 15);
+            }
+            if (e.Key == Key.Left)
+            {
+                double leftCelularActual = Canvas.GetLeft(imgCelular);
+                Canvas.SetLeft(imgCelular, leftCelularActual - 15);
+            }
+            if (e.Key == Key.Right)
+            {
+                double rightCelularActual = Canvas.GetLeft(imgCelular);
+                Canvas.SetLeft(imgCelular, rightCelularActual + 15);
             }
         }
     }
